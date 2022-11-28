fb4754620f35082dfbd70571a45b97b11618e764
choiseU <csw980928@gmail.com>
2022-11-28 15:25:22 +0900

    info correction

--- src/main/java/com/example/fileupload/file/FileController.java
+++ src/main/java/com/example/fileupload/file/FileController.java
@@ -33,19 +33,22 @@ public class FileController {
 //        파일의 데이터
         FileDataVO fileDataVO = new FileDataVO();
         String fileType = fileName.substring(fileName.lastIndexOf(".")+1);
-//        이거는 파일을 구분하기 위함
+
+
+//        이거는 파일을 구분하기 위함(임의로 랜덤으로 뽑게 만듦 추후에 바꿔야함)
         Random r = new Random();
         int ran = r.nextInt(4);
         fileDivisions fileDivision;
-//        임의로 랜덤으로 뽑게 만듦
         switch (ran){case 0: fileDivision = fileDivisions.애니맨_회원; break; case 1: fileDivision = fileDivisions.헬퍼_전환; break; case 2: fileDivision = fileDivisions.헬퍼_정보; break; case 3: fileDivision = fileDivisions.미션_정보; break; case 4: fileDivision = fileDivisions.피드백_정보; break; default: fileDivision = null;}
         fileDataVO.setFileDivision(String.valueOf(fileDivision));
+
+
         fileDataVO.setFileName(fileName);
         fileDataVO.setFileType(fileType);
         fileDataVO.setFileCreatedAt(nowTime);
         fileDataVO.setOperationStatus("waiting");
 
-//        로그인한 정보에 존재할 거 같음. 쿠키나 세션에서 가져와야함
+//        로그인한 정보에 존재할 거 같음. (쿠키나 세션에서 가져와야함 추후에 바꿔야함)
         fileDataVO.setCompanyName("AnyMan");
         fileDataVO.setUploader("김애니");
 
